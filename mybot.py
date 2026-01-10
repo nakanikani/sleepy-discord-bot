0b325b333f36b410dc6916eb491e718c577e4567
nakanikani <kknakanikani@gmail.com>
2026-01-10 17:17:11 +0900

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from threading import Thread
 
+app = Flask('') # 小さなWebサーバーを作る
+
+@app.route('/') # ホームページ（/）にアクセスが来たら...
+def home():
+    return "Bot is running!" # 「動いてるよ！」と返事をする
+
+def run_web():
+    app.run(host='0.0.0.0', port=8080) # サーバーを起動する
+
+def keep_alive():
+    t = Thread(target=run_web) # Botと同時に動くようにセットする
+    t.start()
